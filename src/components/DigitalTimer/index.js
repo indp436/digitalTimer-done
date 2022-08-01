b6b978cf33c82999361c50182abf725e0f370700
indp436 <indp436@gmail.com>
2022-08-01 06:29:46 +0000

    WorkingProperly

---
+++ src/components/DigitalTimer/index.js
@@ -0,0 +1,184 @@
+import {Component} from 'react'
+import './index.css'
+
+class DigitalTimer extends Component {
+  state = {
+    isPaused: false,
+    defaultTimeInMinutes: 25,
+    defaultTimeInSeconds: 0,
+    isReset: false,
+    int: null,
+  }
+
+  componentDidMount() {
+    const {defaultTimeInMinutes} = this.state
+    this.setState({int: defaultTimeInMinutes})
+  }
+
+  increment = () => {
+    const {isPaused, isReset} = this.state
+    if (!isPaused && !isReset) {
+      this.setState(prevState => ({
+        defaultTimeInMinutes: parseInt(prevState.defaultTimeInMinutes) + 1,
+      }))
+      this.componentDidMount()
+    }
+  }
+
+  decrement = () => {
+    const {isPaused, isReset} = this.state
+    if (!isPaused && !isReset) {
+      this.setState(prevState => ({
+        defaultTimeInMinutes: parseInt(prevState.defaultTimeInMinutes) - 1,
+      }))
+      this.componentDidMount()
+    }
+  }
+
+  funCalled = () => {
+    const {isPaused, defaultTimeInMinutes} = this.state
+
+    if (isPaused) {
+      console.log('ksks')
+    } else {
+      this.timerID = setInterval(this.tick, 1000)
+      this.setState({
+        isReset: true,
+      })
+      this.setState(prevState => ({int: prevState.int}))
+    }
+  }
+
+  startOrStopButtonClicked = () => {
+    const {defaultTimeInSeconds, isReset} = this.state
+    this.setState(prevState => ({
+      isPaused: !prevState.isPaused,
+    }))
+    this.funCalled()
+    if (isReset === false) {
+      this.componentDidMount()
+    }
+  }
+
+  tick = () => {
+    const {isPaused, defaultTimeInSeconds, defaultTimeInMinutes} = this.state
+    if (isPaused) {
+      if (defaultTimeInSeconds <= 0) {
+        this.setState(prevState => ({
+          defaultTimeInMinutes: prevState.defaultTimeInMinutes - 1,
+          defaultTimeInSeconds: 60,
+        }))
+      }
+
+      this.setState(prevState => ({
+        defaultTimeInSeconds: prevState.defaultTimeInSeconds - 1,
+      }))
+    } else {
+      console.log('timer stopped', isPaused)
+      clearInterval(this.timerID)
+    }
+  }
+
+  resetButtonClicked = () => {
+    this.setState({
+      isPaused: false,
+      defaultTimeInMinutes: 25,
+      defaultTimeInSeconds: 0,
+      int: null,
+      isReset: false,
+    })
+    this.componentDidMount()
+  }
+
+  render() {
+    const {isPaused, isReset, int} = this.state
+
+    let {defaultTimeInMinutes, defaultTimeInSeconds} = this.state
+
+    if (defaultTimeInSeconds < 10) {
+      defaultTimeInSeconds = ` 0${defaultTimeInSeconds.toString()}`
+    }
+
+    if (defaultTimeInMinutes < 10) {
+      defaultTimeInMinutes = `0${defaultTimeInMinutes.toString()}`
+    }
+
+    return (
+      <div className="bg">
+        <div className="main-container">
+          <h1 className="title">Digital Timer</h1>
+          <div className="sub-container">
+            <div className="left-container">
+              <div className="time-circle">
+                <h1 className="time-in-circle">
+                  {defaultTimeInMinutes}:{defaultTimeInSeconds}
+                </h1>
+                <p className="running">{isPaused ? 'Running' : 'Paused'}</p>
+              </div>
+            </div>
+            <div className="right-container">
+              <div className="buttons-container">
+                <button
+                  className="start-or-stop-button"
+                  type="button"
+                  onClick={this.startOrStopButtonClicked}
+                >
+                  <img
+                    src={
+                      isPaused
+                        ? 'https://assets.ccbp.in/frontend/react-js/pause-icon-img.png'
+                        : 'https://assets.ccbp.in/frontend/react-js/play-icon-img.png'
+                    }
+                    alt="play icon"
+                    className="play-button"
+                  />
+                </button>
+
+                <p className="play-button-text">
+                  {isPaused ? 'Stop' : 'Start'}
+                </p>
+                <button
+                  className="reset-button"
+                  type="button"
+                  onClick={this.resetButtonClicked}
+                >
+                  <img
+                    src="https://assets.ccbp.in/frontend/react-js/reset-icon-img.png "
+                    alt="reset icon"
+                    className="reset-button"
+                  />
+                </button>
+
+                <p className="reset-button-text">Reset</p>
+              </div>
+              <div className="timer-buttons-container">
+                <p className="set-time-text">Set Timer Limit</p>
+                <div className="timer-buttons">
+                  <button
+                    className="span"
+                    type="button"
+                    onClick={this.decrement}
+                  >
+                    -
+                  </button>{' '}
+                  <p className="time-limit-box">
+                    {isReset ? int : defaultTimeInMinutes}
+                  </p>{' '}
+                  <button
+                    className="span"
+                    type="button"
+                    onClick={this.increment}
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default DigitalTimer
